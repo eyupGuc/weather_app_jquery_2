8ab3e69af923cc20730c6d436b901c5b1c63e6cd
eyupGuc <meydanguresi@gmail.com>
2022-10-05 07:36:22 +0300

    first commit

---
+++ weather.js
@@ -0,0 +1,12 @@
+//! Jquery
+// jquery popüler bir js kütüphanesidir. Avantajları:Dom işlemleri, animasyon işlemlerinde çok kulllanılıyor
+
+const formJS = document.querySelectorAll("form")[0];
+const inputJQ = $(".top-banner input").eq(0);
+const msgJQ = $(".top-banner span").eq(0);
+const formJquery = jQuery("form").eq(0);
+const listJQ = $(".cities").eq(0);
+// console.log(formJS);
+// console.log(formJquery);
+console.log(inputJQ);
+// get(index), eq(index)
